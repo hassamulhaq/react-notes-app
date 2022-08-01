1840fd72a8e2ffdfaca248eb20f7bf58d24a18bc
hassamulhaq <developer.hassam@gmail.com>
2022-08-01 06:03:15 +0500

    Fixed Editor react-mde height

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,7 +14,7 @@ function Homepage() {
             <main>
                 <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                     <div className="px-4 py-6 sm:px-0">
-                        <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
+                        <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 box-content">
                             <Split
                                 sizes={[30, 70]}
                                 direction="horizontal"
